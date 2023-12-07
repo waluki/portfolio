a7c42a5bbe96b6165994bb91de16372b43d30704
waluko <iscastillo1964@gmail.com>
2023-12-06 22:32:16 -0300

    version 1.0

---
+++ index.js
@@ -0,0 +1,53 @@
+(function(){
+    let navbar=document.querySelector('#navbar');
+    let hamburger=document.querySelector('#hamburger');
+    hamburger.addEventListener('click',()=>{
+        navbar.classList.toggle('open');
+    });
+
+})();
+
+(function(){
+    let lista=document.querySelector('ul');
+    let hamburger=document.querySelector('#hamburger');
+    hamburger.addEventListener('click',()=>{
+        lista.classList.toggle('close');
+    });
+
+})();
+
+(function () {
+
+    let navbar = document.querySelector('#navbar');
+    let main = document.querySelector('main');
+    let navbarHeight = navbar.getBoundingClientRect().height;
+    let breakpoint = main.offsetTop - navbarHeight;
+    let windowPos;
+    let isFixed = false;
+
+    function updatePos() {
+        windowPos = window.scrollY;
+    }
+
+    function onScroll() {
+
+        updatePos();
+    
+        if (windowPos >= breakpoint && !isFixed) {
+            navbar.classList.remove('open');
+            
+
+            navbar.classList.add('navbar-fixed');
+            main.style.cssText = "margin-top: " + navbarHeight + 'px;';
+            isFixed = true;
+    
+        } else if (windowPos < breakpoint && isFixed) {
+            navbar.classList.remove('navbar-fixed');
+            main.style.cssText = "margin-top: " + 0;
+            isFixed = false;
+        }
+    }
+
+    document.addEventListener('scroll', onScroll);
+
+})();
